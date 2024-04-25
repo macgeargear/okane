66ad1a68768953ba36033cf1e5bd1b462b9cc907
Gear <84141000+macgeargear@users.noreply.github.com>
2024-04-25 22:24:31 +0700

    fix: remove income-statement

--- src/app/api/stocks/income-statement/route.ts
+++
@@ -1,7 +0,0 @@
-import { STOCK_SCREENER_URL, STOCK_STATEMENT_URL } from "@/config";
-import { Fetch } from "@/lib/utils";
-
-export async function GET(req: Request) {
-    const data = await Fetch(`${STOCK_STATEMENT_URL}`)
-  return Response.json("");
-}
