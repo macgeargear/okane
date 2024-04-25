b12269d6e51a274995f425a3554f32536155d749
Gear <84141000+macgeargear@users.noreply.github.com>
2024-04-25 22:50:24 +0700

    fix remove stock performance

--- src/app/api/stocks/sectors-performance/route.ts
+++
@@ -1,8 +0,0 @@
-import { SECTOR_PERFORMANCE_URL } from "@/config";
-import { Fetch } from "@/lib/utils";
-
-export async function GET(req: Request) {
-  const data = await Fetch(`${SECTOR_PERFORMANCE_URL}`);
-
-  return Response.json(data);
-}
