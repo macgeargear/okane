4904671356b3d65719f0cbe1b911fde647f011b5
Gear <84141000+macgeargear@users.noreply.github.com>
2024-04-25 22:42:17 +0700

    fix: remove stock score api

--- src/app/api/stocks/score/route.ts
+++
@@ -1,8 +0,0 @@
-import { STOCK_SCREENER_URL } from "@/config";
-import { Fetch } from "@/lib/utils";
-
-export async function GET(req: Request) {
-  const data = await Fetch(`${STOCK_SCREENER_URL}`);
-
-  return Response.json(data);
-}
