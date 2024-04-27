e8ac4730fabdd8ba0299876c68077d1c92fbd7fc
Gear <84141000+macgeargear@users.noreply.github.com>
2024-04-27 15:30:44 +0700

    chore

--- src/hooks/query/stock/useStockScreener.ts
+++ src/hooks/query/stock/useStockScreener.ts
@@ -1,14 +1,15 @@
-import { stockScreenReq } from "../../../lib/schema/stock/index";
+//TODO: fix the queryFn to use the stockScreenReq
+
 import { StockScreenerResult } from "@/types/stocks";
 import { useQuery } from "@tanstack/react-query";
 import { Fetch } from "@/lib/utils";
 
-export default function useStockScreener(stockScreenReq?: stockScreenReq) {
+export default function useStockScreener(stockScreenReq?: string) {
   return useQuery<StockScreenerResult[]>({
     queryKey: ["stock", stockScreenReq],
     queryFn: () =>
       Fetch("/api/stocks/screener", {
-        body: JSON.stringify(stockScreenReq),
+        // body: JSON.stringify(stockScreenReq),
       }),
   });
 }
