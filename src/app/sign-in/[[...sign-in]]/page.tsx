8fab255609f82fe95bfc1e60f5a82cd79c09f98b
Gear <84141000+macgeargear@users.noreply.github.com>
2024-04-27 15:25:03 +0700

    fix: remove old auth page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="container flex items-center justify-center">
-      <SignIn path="/sign-in" />
-    </div>
-  );
-}
